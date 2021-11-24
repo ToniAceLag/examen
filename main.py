97ed3583f99be8f55151a6ba35fdaf1bb8a53979
ToniAceLag <animationstudioofficial@gmail.com>
2021-11-24 11:51:14 +0100

    I have add the library

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-# This is a sample Python script.
+def main():
+    pass
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
