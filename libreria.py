1bd1d6782798fa9731f18ea1df5035da7e3f0ce6
ToniAceLag <animationstudioofficial@gmail.com>
2021-12-01 12:27:21 +0100

    Added the ":" to some sentences and a new function to valid a number

--- libreria.py
+++ libreria.py
@@ -2,16 +2,13 @@
 def list_natural_numbers():
     sum = 0
     naturals = list()
-    num = valid_number(0, "Introdueix un nombre natural o cero per sortir")
+    num = valid_number(0, "Introdueix un nombre natural o cero per sortir:")
     while num != 0:
         naturals.append(num)
-        num = valid_number(0, "Introdueix un nombre natural o cero per sortir")
+        num = valid_number(0, "Introdueix un nombre natural o cero per sortir:")
     for x in naturals:
         sum += x
     print(sum)
-
-
-
 
 
 #Ejercicio 2:
@@ -19,6 +16,7 @@ def menu(msg):
     print("//////////////////////////////")
     print(msg)
     print("//////////////////////////////")
+
 
 #Ejercicio 3:
 def h_library():
@@ -44,6 +42,7 @@ def h_library():
         "ISBN:":isbn
     }
     print(diccionario)
+
 
 def valid_number(val, msg):
     num = int(input(msg))
